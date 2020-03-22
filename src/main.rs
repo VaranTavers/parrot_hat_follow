1414450123203fb49dc1bffcf05fd02e060aa677
VaranTavers <tasnadi98@freemail.hu>
2020-03-22 18:28:55 +0200

    Compiles, needs testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::{thread, io};
+use rust_drone_follow::HatFollower;
+use rust_drone_follow::detectors::naive_detector::NaiveDetector;
+use rust_drone_follow::hat_file_reader::read_file;
+use rust_drone_follow::filters::no_filter::NoFilter;
+
+mod parrot_controller;
+
+use parrot_controller::ParrotController;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+fn read_int() -> i32 {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    parse_input!(input_line.trim(), i32)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (mut sx, rx) = std::sync::mpsc::channel();
+    let (_, hat) = read_file("kek.hat");
+
+    let handle = thread::spawn(|| {
+        let mut hf = HatFollower::new(
+            NaiveDetector::new(hat),
+            ParrotController::new(),
+            NoFilter::new(),
+            Some(rx)
+        );
+
+        hf.run();
+    });
+
+    loop {
+        let i = read_int();
+        if i == 0 {
+            sx.send(0);
+            break;
+        }
+    }
+
+    handle.join().unwrap();
 }
---
+++ src/parrot_controller.rs
@@ -0,0 +1,90 @@
+use rust_drone_follow::traits::Controller;
+use parrot_ar_drone::Drone;
+
+use std::thread;
+use std::time::Duration;
+
+pub struct ParrotController {
+    drone: Option<Drone>,
+}
+
+impl ParrotController {
+    pub fn new() -> ParrotController {
+        ParrotController {
+            drone: Some(Drone::new()),
+        }
+    }
+}
+
+impl Controller for ParrotController {
+    fn init(&mut self) {
+        let mut drone = self.drone.take().unwrap();
+        match drone.startup() {
+            Ok(()) => {
+                thread::sleep(Duration::from_secs(3));
+            }
+            Err(s) => {
+                panic!(s);
+            }
+        }
+        self.drone.replace(drone);
+    }
+
+    fn shutdown(&mut self) {
+        self.drone.take().unwrap();
+    }
+
+    fn takeoff(&mut self) {
+        let mut drone = self.drone.take().unwrap();
+        drone.takeoff();
+        drone.mov_up(1.0);
+        thread::sleep(Duration::from_secs(2));
+        drone.stop();
+        self.drone.replace(drone);
+    }
+
+    fn land(&mut self) {
+        let mut drone = self.drone.take().unwrap();
+        drone.land();
+        self.drone.replace(drone);
+    }
+
+    fn move_all(&mut self, left_right: f64, back_front: f64, down_up: f64, turn_left_right: f64) {
+        let mut drone = self.drone.take().unwrap();
+        drone.mov(
+            left_right as f32,
+            back_front as f32,
+            down_up as f32,
+            turn_left_right as f32
+        );
+        self.drone.replace(drone);
+    }
+
+    fn stop(&mut self) {
+        let mut drone = self.drone.take().unwrap();
+        drone.stop();
+        self.drone.replace(drone);
+    }
+
+    fn get_video_height(&self) -> usize {
+        368
+    }
+
+    fn get_video_width(&self) -> usize {
+        640
+    }
+
+    fn get_opencv_url(&self) -> String {
+        String::from("tcp://192.168.1.1:5555")
+    }
+
+    fn get_kv(&self) -> f64 {
+        // NEEDS TESTING
+        0.01
+    }
+
+    fn get_ka(&self) -> f64 {
+        // NEEDS TESTING
+        0.01
+    }
+}
