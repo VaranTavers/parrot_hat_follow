34613a2f97a7bb6dde6290e162e066abe9c818f8
VaranTavers <tasnadi98@freemail.hu>
2020-06-01 16:58:08 +0300

    updated to hat_follow 0.6.0

---
+++ src/simulation/windtactics/random_wind.rs
@@ -0,0 +1,45 @@
+use crate::simulation::traits::WindTactic;
+
+pub struct PeriodicWind {
+    speed_x: f64,
+    speed_y: f64,
+    active: usize,
+    inactive: usize,
+    frame_num: usize,
+}
+
+impl PeriodicWind {
+    pub fn new(speed_x: f64, speed_y: f64, active: usize, inactive: usize) -> PeriodicWind {
+        PeriodicWind {
+            speed_x,
+            speed_y,
+            active,
+            inactive,
+            frame_num: 0,
+        }
+    }
+
+    pub fn new_polar(force: f64, angle: f64, active: usize, inactive: usize) -> PeriodicWind {
+        PeriodicWind {
+            speed_x: force * angle.sin(),
+            speed_y: force * angle.cos(),
+            active,
+            inactive,
+            frame_num: 0,
+        }
+    }
+}
+
+impl WindTactic for PeriodicWind {
+    fn get_wind(&mut self) -> (f64, f64) {
+        self.frame_num += 1;
+        if self.frame_num < self.active {
+            return (self.speed_x, self.speed_y);
+        }
+        if self.frame_num > self.active + self.inactive {
+            self.frame_num = 0;
+            println!("Wind on!");
+        }
+        (0.0, 0.0)
+    }
+}
